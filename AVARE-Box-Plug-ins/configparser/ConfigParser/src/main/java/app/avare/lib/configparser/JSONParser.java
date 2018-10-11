95eb9b27e58bd7586c1f701ca359de87f3998f3e
Sascha Alpers <alpers@fzi.de>
2018-10-11 14:10:08 +0200

    ConfigParser library
    ConfigParser library to read the JSON configuration file within the individual plug-ins.

---
+++ AVARE-Box-Plug-ins/configparser/ConfigParser/src/main/java/app/avare/lib/configparser/JSONParser.java
@@ -0,0 +1,111 @@
+package app.avare.lib.configparser;
+
+/*
+        Copyright 2016-2018 AVARE project team
+
+        AVARE-Project was financed by the Baden-Württemberg Stiftung gGmbH (www.bwstiftung.de).
+        Project partners are FZI Forschungszentrum Informatik am Karlsruher
+        Institut für Technologie (www.fzi.de) and Karlsruher
+        Institut für Technologie (www.kit.edu).
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class JSONParser {
+    private FileReader fr;
+    private String config;
+    private JSONObject configJSON;
+
+    public JSONParser() {
+        this.fr = new FileReader();
+        this.config = fr.readFile("avare_demo_config.json");
+        try {
+            this.configJSON = new JSONObject(config);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JSONObject getSettings() {
+        try {
+            JSONArray categories = this.configJSON.getJSONArray("categories");
+            JSONObject category = categories.getJSONObject(0);
+            JSONObject settings = category.getJSONObject("settings");
+            return settings;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public JSONArray getContactsSettings(String type) {
+        try {
+            JSONObject settings = this.getSettings();
+            JSONObject contacts = settings.getJSONObject("contacts");
+            JSONObject filtersettings = contacts.getJSONObject("filterSettings");
+            if (type.equals("vertical")) {
+                JSONArray vertical = filtersettings.getJSONArray("vertical");
+                return vertical;
+            } else if (type.equals("horizontal")) {
+                JSONArray horizontal = filtersettings.getJSONArray("horizontal");
+                return horizontal;
+
+            } else {
+                Log.i("JSON Parser", "Wrong usage of getContactsSettings");
+                return null;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public int getLocationRadius() {
+        try {
+            JSONObject settings = this.getSettings();
+            JSONObject location = settings.getJSONObject("location");
+            JSONObject filtersettings = location.getJSONObject("filterSettings");
+
+            try {
+                int radius = filtersettings.getInt("distance");
+                return radius;
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /*
+    * Utility function to check whether the JSON Array contains an element or not
+    */
+    public static boolean jSONArrayContains(JSONArray array, String element) throws JSONException {
+        if(array.length() == 0) {
+            return false;
+        }
+        for(int i = 0; i < array.length(); i++) {
+            if (array.get(i).toString().equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ AVARE-Box-Plug-ins/configparser/README.md
@@ -0,0 +1,27 @@
+ConfigParser
+-----------
+
+The ConfigParser creates a library that provides methods of parsing JSON-Files and allows the AVARE-Box-Plug-ins to read and use the settings defined in the config file
+ 
+ To update a newer version of the aar for the Plug-ins, simply build this updated project and copy the resulting aar (ConfigParser-debug.aar) into the ConfigParser directory in the specific Plug-in project. Finally, rename the aar into ConfigParser.aar.
+
+## License
+        Copyright 2016-2018 AVARE project team
+
+        AVARE-Project was financed by the Baden-Württemberg Stiftung gGmbH (www.bwstiftung.de).
+        Project partners are FZI Forschungszentrum Informatik am Karlsruher
+        Institut für Technologie (www.fzi.de) and Karlsruher
+        Institut für Technologie (www.kit.edu).
+
+        Files under this folder (and the subfolders) with "Created by AVARE Project ..."-Notice
+		are our work and licensed under Apache Licence, Version 2.0"
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
