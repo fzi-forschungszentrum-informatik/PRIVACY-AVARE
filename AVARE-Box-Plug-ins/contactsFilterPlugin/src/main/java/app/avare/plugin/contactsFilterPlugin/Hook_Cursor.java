de24ead7df6f4b5ea905c74852bd3803c2c2756e
Sascha Alpers <alpers@fzi.de>
2018-12-06 08:57:26 +0100

    Improved Contacts Filter

--- AVARE-Box-Plug-ins/contactsFilterPlugin/src/main/java/app/avare/plugin/contactsFilterPlugin/Hook_Cursor.java
+++ AVARE-Box-Plug-ins/contactsFilterPlugin/src/main/java/app/avare/plugin/contactsFilterPlugin/Hook_Cursor.java
@@ -20,15 +20,19 @@ package app.avare.plugin.contactsFilterPlugin;
 */
 
 import android.database.Cursor;
+import android.database.CursorJoiner;
 import android.database.CursorWrapper;
+import android.database.MatrixCursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 import app.avare.lib.configparser.JSONParser;
-
 
 public class Hook_Cursor {
 
@@ -39,7 +43,6 @@ public class Hook_Cursor {
     public static Cursor hook(Object thiz, Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 
         Log.i("Hook Cursor", "Uri: " + uri);
-
 
         String[] newProjection;
         if (projection != null) {
@@ -66,20 +69,67 @@ public class Hook_Cursor {
             newProjection = null;
         }
 
+        Cursor structuredNameCursor = backup(thiz, ContactsContract.Data.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, ContactsContract.Data.CONTACT_ID}, ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME + " != ?", new String[]{ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, "null"}, ContactsContract.Data.CONTACT_ID);
+
+        //structuredNameCursor.moveToFirst();
+        while (structuredNameCursor.moveToNext()) {
+            Log.i("STRUCTURED NAME CURSOR", "ID: " + structuredNameCursor.getString(structuredNameCursor.getColumnIndex(ContactsContract.Data.CONTACT_ID)));
+            Log.i("STRUCTURED NAME CURSOR", "GIVEN_NAME: " + structuredNameCursor.getString(structuredNameCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME)));
+        }
 
         Cursor cursor = backup(thiz, uri, newProjection, selection, selectionArgs, sortOrder);
 
+        String contactIDKey;
+        if (cursor.getColumnIndex(ContactsContract.Contacts._ID) != -1) {
+            contactIDKey = ContactsContract.Contacts._ID;
+        } else {
+            contactIDKey = ContactsContract.Data.CONTACT_ID;
+        }
+
+        String[] newColumns = new String[cursor.getColumnCount() + 1];
+        for (int i = 0; i < cursor.getColumnCount(); i++) {
+            newColumns[i] = cursor.getColumnName(i);
+        }
+        newColumns[cursor.getColumnCount()] = ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME;
+        MatrixCursor joinedCursor = new MatrixCursor(newColumns);
+
+        CursorJoiner joiner = new CursorJoiner(cursor, new String[]{contactIDKey}, structuredNameCursor, new String[]{ContactsContract.Data.CONTACT_ID});
+        for (CursorJoiner.Result res : joiner) {
+            switch (res) {
+                case LEFT:
+                    Log.i("CURSOR JOINER", "left");
+
+                    break;
+                case RIGHT:
+                    Log.i("CURSOR JOINER", "right");
+
+                    break;
+                case BOTH:
+                    Object[] columns = new Object[cursor.getColumnCount() + 1];
+                    for (int i = 0; i < cursor.getColumnCount(); i++) {
+                        columns[i] = cursor.getString(i);
+                    }
+                    columns[cursor.getColumnCount()] = structuredNameCursor.getString(structuredNameCursor.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME));
+                    joinedCursor.addRow(columns);
+                    Log.i("CURSOR JOINER", "both");
+                    break;
+            }
+        }
+
+        for (int i = 0; i < joinedCursor.getColumnCount(); i++) {
+            Log.i("JOINED CURSOR", joinedCursor.getColumnName(i));
+        }
+
         JSONParser jp = new JSONParser();
 
-
-        HookCursor hc = new HookCursor(cursor, jp);
+        HookCursor hc = new HookCursor(joinedCursor, jp);
         Log.d("CONTACTS HOOK", "cursor Hooked!!");
 
         return hc;
     }
 
     public static Cursor backup(Object thiz, Uri uri, String[] projection, String selection, String[] selectionAgs, String sortOrder) {
-        Log.d("BACKUP", "Unfortunately the backup method was called");
+        //Log.d("BACKUP", "Unfortunately the backup method was called");
 
         return null;
     }
@@ -90,6 +140,9 @@ public class Hook_Cursor {
         JSONArray vertical;
         JSONArray horizontal;
 
+        private String[] notToFilterArray = new String[]{"_id"};
+        private ArrayList<String> notToFilter = new ArrayList<String>();
+
         private HookCursor(Cursor c, JSONParser config) {
 
             super(c);
@@ -98,31 +151,35 @@ public class Hook_Cursor {
             this.horizontal = this.parser.getContactsSettings("horizontal");
             Log.d("constructor", "Private class HookCursor Constructed!");
 
+            for (String s : notToFilterArray) {
+                notToFilter.add(s);
+            }
+
         }
 
         @Override
         public String getString(int columnIndex) {
+            Log.i("HOOK CURSOR", "Calling Cursor getString()");
+            try {
+                if (JSONParser.jSONArrayContains(this.vertical, super.getColumnName(columnIndex)) || this.notToFilter.contains(super.getColumnName(columnIndex))) {
+                    Log.i("VERTICAL", "Config contains " + super.getColumnName(columnIndex) + ", returning");
+                    return super.getString(columnIndex);
+                } else {
+                    Log.i("VERTICAL", "Config doesn't contain " + super.getColumnName(columnIndex) + ", not returning");
+                    if (super.getColumnName(columnIndex).equals(ContactsContract.Contacts.DISPLAY_NAME) || this.notToFilter.contains(super.getColumnName(columnIndex))) {
+                        String givenName = super.getString(super.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME));
+                        Log.i("VERTICAL", "Accessing DISPLAY_NAME, returning GIVEN_NAME: " + givenName);
+                        return givenName;
+                    }
+                }
 
-            //Log.i("Hook Cursor", "Common Data Kinds: " + ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-            //Log.i(("Hook Cursor"), "Contacts: " + ContactsContract.Contacts.DISPLAY_NAME);
-            //Log.d("HookCursor", "Using HookCursor");
-          /*  if (columnIndex == super.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)) {
-                String givenName = super.getString(super.getColumnIndex(ContactsContract.
-                        CommonDataKinds.StructuredName.GIVEN_NAME));
-                return givenName;
-            }*/
 
-
-            if (columnIndex == super.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME)) {
-                return "";
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
-            String s = super.getString(columnIndex);
-            if (columnIndex == super.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)) {
-                String[] splits = s.split(" ");
-                return splits[0];
-            }
-            return super.getString(columnIndex);
+            return "";
+
         }
 
         @Override
@@ -133,9 +190,12 @@ public class Hook_Cursor {
                 if (next) {
                     Log.i("HOOK CURSOR", "Querying ID");
                     for (int i = 0; i < this.getColumnCount(); i++) {
-                        Log.i("HOOK CURSOR table", this.getColumnName(i) + ": " + this.getString(i));
+                        //Log.i("HOOK CURSOR table", this.getColumnName(i) + ": " + this.getString(i));
                     }
-                    String currentID = this.getString(this.getColumnIndex(ContactsContract.Data.CONTACT_ID));
+                    String currentID = "";
+                    if (this.getColumnIndex(ContactsContract.Data.CONTACT_ID) > -1) {
+                        currentID = this.getString(this.getColumnIndex(ContactsContract.Data.CONTACT_ID));
+                    }
                     if (currentID == null || currentID.equals("")) {
                         currentID = this.getString(this.getColumnIndex(ContactsContract.Contacts._ID));
                     }
