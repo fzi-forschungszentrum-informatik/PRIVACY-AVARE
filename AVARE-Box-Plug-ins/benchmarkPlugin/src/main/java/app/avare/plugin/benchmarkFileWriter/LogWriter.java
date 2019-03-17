248285d7d92d302d59c2e2d36a7b7209773bc022
Christian Zielke <zielke@fzi.de>
2019-03-17 20:21:34 +0100

    added screenshot for benchmarkPlugin

---
+++ AVARE-Box-Plug-ins/benchmarkPlugin/src/main/java/app/avare/plugin/benchmarkFileWriter/LogWriter.java
@@ -0,0 +1,73 @@
+package app.avare.plugin.benchmarkFileWriter;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Calendar;
+
+/**
+ * Class to write the log files to the specified location.
+ */
+public class LogWriter {
+
+    private static String LOG_FILE_NAME = "/benchmarkLog.txt";
+
+    /**
+     * Appends a new log entry to the log file.
+     *
+     * @param message
+     * @return true if success
+     *         false an exception occured during writing
+     */
+    public boolean addLine(String message) {
+        File sdcard = Environment.getExternalStorageDirectory();
+        File f = new File( Environment.getExternalStorageDirectory().getAbsolutePath() + LOG_FILE_NAME);
+        StringBuilder sb = new StringBuilder();
+        if (f.exists()) {
+            BufferedReader br = null;
+            try {
+                br = new BufferedReader(new java.io.FileReader(f));
+                String line;
+                while((line = br.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        sb.append(Calendar.getInstance().getTime().toString() + "    " + extractPackage() + "    " + message + "\n");
+        try {
+            BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(f));
+            writer.write(sb.toString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    private String extractPackage() {
+        StackTraceElement[] stackTrace;
+        try {
+            throw new NullPointerException();
+        } catch (Exception e) {
+            stackTrace = e.getStackTrace();
+        }
+        for (StackTraceElement stackTraceElement : stackTrace) {
+            String packageName = stackTraceElement.getClassName().substring(0, stackTraceElement.getClassName().lastIndexOf("."));
+            if (!packageName.startsWith("app.avare") && !packageName.startsWith("android")) {
+                return packageName;
+            }
+        }
+        return null;
+    }
+}
