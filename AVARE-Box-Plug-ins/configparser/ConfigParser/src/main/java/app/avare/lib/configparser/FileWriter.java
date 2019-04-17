7f8e14b101c8ec7b6ff8e64df5a8dc5e98944c20
Christian Zielke <zielke@fzi.de>
2019-04-17 14:53:07 +0200

    added license comments

--- AVARE-Box-Plug-ins/configparser/ConfigParser/src/main/java/app/avare/lib/configparser/FileWriter.java
+++ AVARE-Box-Plug-ins/configparser/ConfigParser/src/main/java/app/avare/lib/configparser/FileWriter.java
@@ -7,6 +7,27 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 
+/*
+ * Created by AVARE Project
+ */
+
+/*
+        Copyright 2016-2019 AVARE project team
+        AVARE-Project was financed by the Baden-Württemberg Stiftung gGmbH (www.bwstiftung.de).
+        Project partners are FZI Forschungszentrum Informatik am Karlsruher
+        Institut für Technologie (www.fzi.de) and Karlsruher
+        Institut für Technologie (www.kit.edu).
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+
 /**
  * Class to write the modified JSON File to the specified location.
  */
